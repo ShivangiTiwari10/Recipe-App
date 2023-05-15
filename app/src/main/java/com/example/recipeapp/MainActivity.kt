6aee3f688186f1d68d82e21c8ce5610757769565
Shivangi <shivangit1111@gmail.com>
2023-05-15 19:24:16 +0530

    chore: add new Activity

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.recipeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recipeapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        supportActionBar?.hide()
+        activityScope.launch {
+
+            delay(3000)
+
+            val intent = Intent(this@MainActivity, ShowRecipe::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         setContentView(binding.root)
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
     }
 }
