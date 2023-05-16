6893582041fb80e6aa6b9cf333ee982a406cad60
Shivangi <shivangit1111@gmail.com>
2023-05-16 12:41:48 +0530

    feat: implement binding

--- app/src/main/java/com/example/recipeapp/ShowRecipe.kt
+++ app/src/main/java/com/example/recipeapp/ShowRecipe.kt
@@ -2,10 +2,15 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipeapp.databinding.ActivityShowRecipeBinding
 
 class ShowRecipe : AppCompatActivity() {
+
+    private lateinit var binding: ActivityShowRecipeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_recipe)
+
+        binding = ActivityShowRecipeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
