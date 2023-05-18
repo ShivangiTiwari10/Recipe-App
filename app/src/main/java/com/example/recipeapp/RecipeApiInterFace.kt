0e075137a245da4bb2aafe71aba1c6f8c2e8b86a
Shivangi <shivangit1111@gmail.com>
2023-05-18 17:25:33 +0530

    feat: create query params

---
+++ app/src/main/java/com/example/recipeapp/RecipeApiInterFace.kt
@@ -0,0 +1,14 @@
+package com.example.recipeapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RecipeApiInterFace {
+    @GET("recipes/findByIngredients")
+
+    fun getRecipeData(
+    @Query("ingredients") ingredients: String,
+    @Query("apiKey") apiKey: String): Call<recipeData>
+
+}
