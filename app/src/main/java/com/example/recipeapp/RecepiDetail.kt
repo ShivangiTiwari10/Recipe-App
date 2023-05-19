29eb74c84c02f2d4c49f7bd5a1d29fc1063f55c4
Shivangi <shivangit1111@gmail.com>
2023-05-19 14:30:16 +0530

    feat: get recipe detail from home fragment

--- app/src/main/java/com/example/recipeapp/RecepiDetail.kt
+++ app/src/main/java/com/example/recipeapp/RecepiDetail.kt
@@ -1,11 +1,36 @@
 package com.example.recipeapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipeapp.databinding.ActivityRecepiDetailBinding
+import com.squareup.picasso.Picasso
 
 class RecepiDetail : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRecepiDetailBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recepi_detail)
+
+        binding = ActivityRecepiDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val image = intent.getStringExtra("Image")
+        val title = intent.getStringExtra("title")
+        val mised = intent.getStringExtra("missed")
+        val ussed = intent.getStringExtra("used")
+        val likes = intent.getStringExtra("likes")
+        val iid = intent.getStringExtra("Id")
+
+
+        binding.txtTitle.text = title
+        Picasso.get().load(image).into(binding.recipeImage)
+        binding.textMissed.text = "MissedIngredient:$mised"
+        binding.textUsed.text = "UsedIngredient:$ussed"
+        binding.likes.text = "Likes:$likes"
+        binding.Id.text = "ID:$iid"
+
     }
 }
