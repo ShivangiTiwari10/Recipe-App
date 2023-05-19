266ea7a8a18eab0f17443fed28848032686b065c
Shivangi <shivangit1111@gmail.com>
2023-05-19 14:28:27 +0530

    feat: set listener in recyclerView

--- app/src/main/java/com/example/recipeapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/recipeapp/fragment/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.recipeapp.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recipeapp.RecepiDetail
 import com.example.recipeapp.RecipeAdapter
 import com.example.recipeapp.RecipeApiInterFace
 import com.example.recipeapp.databinding.FragmentHomeBinding
@@ -50,15 +52,29 @@ class HomeFragment : Fragment() {
 
                 binding.myRecycler.layoutManager = LinearLayoutManager(requireContext())
 
+                myadapter.setOnItemClickListener(object : RecipeAdapter.onItemClickListener {
+                    override fun onItemClicking(position: Int) {
+                        val clickedRecipe = recipeList[position]
+
+
+                        val intent = Intent(requireContext(), RecepiDetail::class.java)
+
+                        intent.putExtra("Image", clickedRecipe.image)
+                        intent.putExtra("title", clickedRecipe.title)
+                        intent.putExtra("missed", clickedRecipe.missedIngredientCount.toString())
+                        intent.putExtra("used", clickedRecipe.usedIngredientCount.toString())
+                        intent.putExtra("likes", clickedRecipe.likes.toString())
+                        intent.putExtra("Id", clickedRecipe.id.toString())
+                        startActivity(intent)
+                    }
+                })
+
             }
 
             override fun onFailure(call: Call<recipeData?>, t: Throwable) {
                 Log.d("MyRetrofitActivity", "onFailure ${t.message}")
             }
         })
-
-
-
         return binding.root
     }
 
