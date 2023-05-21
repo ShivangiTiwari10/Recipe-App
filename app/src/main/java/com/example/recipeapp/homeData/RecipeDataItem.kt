59d891dfafd72ca8e4b5fd7335bedac088603293
Shivangi <shivangit1111@gmail.com>
2023-05-21 09:49:39 +0530

    chore: create new activity

---
+++ app/src/main/java/com/example/recipeapp/homeData/RecipeDataItem.kt
@@ -0,0 +1,29 @@
+package com.example.recipeapp.homeData
+
+data class recipeDataItem(
+    val id: Int,
+    val image: String,
+    val imageType: String,
+    val likes: Int,
+    val missedIngredientCount: Int,
+    val missedIngredients: List<MissedIngredient>,
+    val title: String,
+    val unusedIngredients: List<Any>,
+    val usedIngredientCount: Int,
+    val usedIngredients: List<UsedIngredient>
+)
+
+data class MissedIngredient(
+    val aisle: String,
+    val amount: Double,
+    val extendedName: String,
+    val id: Int,
+    val image: String,
+    val meta: List<String>,
+    val name: String,
+    val original: String,
+    val originalName: String,
+    val unit: String,
+    val unitLong: String,
+    val unitShort: String
+)
--- app/src/main/java/com/example/recipeapp/recipeData.kt
+++
@@ -1,3 +0,0 @@
-package com.example.recipeapp
-
-class recipeData : ArrayList<recipeDataItem>()
