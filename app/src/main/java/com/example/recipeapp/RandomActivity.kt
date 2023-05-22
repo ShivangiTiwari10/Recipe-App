4d59c73320f7bcd34bb4c2c4b02b82b33f46e122
Shivangi <shivangit1111@gmail.com>
2023-05-22 17:50:41 +0530

    feat: get random api data

--- app/src/main/java/com/example/recipeapp/RandomActivity.kt
+++ app/src/main/java/com/example/recipeapp/RandomActivity.kt
@@ -3,6 +3,7 @@ package com.example.recipeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recipeapp.databinding.ActivityRandomBinding
+import com.squareup.picasso.Picasso
 
 class RandomActivity : AppCompatActivity() {
 
@@ -13,5 +14,22 @@ class RandomActivity : AppCompatActivity() {
 
         binding = ActivityRandomBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val image = intent.getStringExtra("Image")
+        val title = intent.getStringExtra("Title")
+        val detail = intent.getStringExtra("Detail")
+        val prize = intent.getStringExtra("PrizeServing")
+        val sumery = intent.getStringExtra("summery")
+        val readyy = intent.getStringExtra("ready")
+
+
+        binding.scrollViewtxt.text = detail
+        binding.recipeTitle.text = title
+        binding.prize.text = prize
+        binding.summarytext.text = sumery
+        binding.readyinminutes.text = readyy
+
+
+        Picasso.get().load(image).into(binding.randomImage)
     }
 }
