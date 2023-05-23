cc0744f9ce20209c174ab2ad9840d3551dc01fea
Shivangi <shivangit1111@gmail.com>
2023-05-23 14:50:50 +0530

    feat: create interface for home fragment

--- app/src/main/java/com/example/recipeapp/interfaces/RecipeApiInterFace.kt
+++ app/src/main/java/com/example/recipeapp/interfaces/RecipeApiInterFace.kt
@@ -1,16 +1,17 @@
 package com.example.recipeapp.interfaces
 
-import com.example.recipeapp.homeData.recipeData
+import com.example.recipeapp.homeData.ComplexSearch
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RecipeApiInterFace {
-    @GET("recipes/findByIngredients")
-
+    @GET("recipes/complexSearch")
     fun getRecipeData(
-        @Query("ingredients") ingredients: String,
+        @Query("apiKey") apiKey: String,
 //        @Query("number") number: Int,
-        @Query("apiKey") apiKey: String
-    ): Call<recipeData>
+        @Query("type") type: String,
+        @Query("number") number: Int
+
+    ): Call<ComplexSearch>
 }
